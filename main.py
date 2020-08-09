4597723c0d35d51454e5a4460674ddceea804c2f
trapwired <dominic.weibel@protonmail.ch>
2020-08-09 16:04:58 +0200

    added Mike

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import json
-import numpy as np
-import pandas as pd
-from os import path
-from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 import matplotlib.pyplot as plt
 
-names = ['Chris Buob', 'Valentin Anklin', 'Philipp Hadjimina', 'Fluffy Octopus', 'David', 'Mischa Buob']
+names = ['Chris Buob', 'Valentin Anklin', 'Philipp Hadjimina', 'Fluffy Octopus', 'David', 'Mischa Buob', 'Mike Keller']
 WHO = names[0]
 USE_SW = False
 
